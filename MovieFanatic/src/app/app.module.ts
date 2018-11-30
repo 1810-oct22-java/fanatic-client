55d5c466485ea3ba3ecf674160257295d068b67e
PGerringer <PGerringer@gmail.com>
2018-11-30 13:29:24 -0500

    live pull of movie data from API

--- MovieFanatic/src/app/app.module.ts
+++ MovieFanatic/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { Http } from '@angular/http';
+//import { Http } from '@angular/http';
 //import { Tabulator } from 'tabulator-tables/dist/js/tabulator.min.js';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieViewComponent } from './components/movie-view/movie-view.component';
 import { HeaderComponent } from './components/header/header.component';
-import { MovieAPI } from './models/movieAPI';
 
 @NgModule({
   declarations: [
@@ -23,8 +22,6 @@ import { MovieAPI } from './models/movieAPI';
   //  Tabulator
   ],
   providers: [ 
-    MovieAPI,
-    Http
   ],
   bootstrap: [AppComponent]
 })
