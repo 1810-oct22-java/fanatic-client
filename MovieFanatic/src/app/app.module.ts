f8672c1d764ce5bfe716fbaf2c4809f7fc02922b
bockdavidson <dbock1994@gmail.com>
2018-11-30 13:49:09 -0500

    updated actor

--- MovieFanatic/src/app/app.module.ts
+++ MovieFanatic/src/app/app.module.ts
@@ -7,13 +7,16 @@ import { Http } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieViewComponent } from './components/movie-view/movie-view.component';
+import { ActorViewComponent } from './components/actor-view/actor-view.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MovieAPI } from './models/movieAPI';
+import { Actor } from './models/actor';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieViewComponent,
+    ActorViewComponent,
     HeaderComponent
   ],
   imports: [
@@ -24,6 +27,7 @@ import { MovieAPI } from './models/movieAPI';
   ],
   providers: [ 
     MovieAPI,
+    Actor,
     Http
   ],
   bootstrap: [AppComponent]
--- MovieFanatic/src/app/models/actor.ts
+++ MovieFanatic/src/app/models/actor.ts
@@ -1,7 +1,11 @@
+import { SafeStyle } from "@angular/platform-browser";
+
 export class Actor {
+    id: string;
     firstName: string;
     lastName: string;
     age: number;
     gender: string;
     birthDate: Date;
+    pic_path:string;
 }
