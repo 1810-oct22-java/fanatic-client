57ac62661228c4947844a3d97f0862af72f5ebe5
Sean Young <seanpyoung123@gmail.com>
2018-12-02 16:20:44 -0500

    First addition of profile-view, edit-view, and user-view... User-view has not been worked on yet.

--- MovieFanatic/src/app/app-routing.module.ts
+++ MovieFanatic/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MovieViewComponent } from 'src/app/components/movie-view/movie-view.component';
+import { ProfileViewComponent } from 'src/app/components/profile-view/profile-view.component';
+import { EditProfileComponent } from 'src/app/components/edit-profile/edit-profile.component';
 
 const routes: Routes = [
-  { path:'', redirectTo:'movie/424694', pathMatch: 'full' },
-  { path:'movie/:id', component: MovieViewComponent },
+  { path: '', redirectTo: 'movie/424694', pathMatch: 'full' },
+  { path: 'movie/:id', component: MovieViewComponent },
+  { path: 'profile-view', component: ProfileViewComponent},
+  { path: 'edit-profile', component: EditProfileComponent}
 ];
 
 @NgModule({
--- MovieFanatic/src/app/app.module.ts
+++ MovieFanatic/src/app/app.module.ts
@@ -2,19 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { Http } from '@angular/http';
-//import { Tabulator } from 'tabulator-tables/dist/js/tabulator.min.js';
+// import { Tabulator } from 'tabulator-tables/dist/js/tabulator.min.js';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieViewComponent } from './components/movie-view/movie-view.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MovieAPI } from './models/movieAPI';
+import { ProfileViewComponent } from './components/profile-view/profile-view.component';
+import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieViewComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProfileViewComponent,
+    EditProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +26,7 @@ import { MovieAPI } from './models/movieAPI';
     AppRoutingModule
   //  Tabulator
   ],
-  providers: [ 
+  providers: [
     MovieAPI,
     Http
   ],
