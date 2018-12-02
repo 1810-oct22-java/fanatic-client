e3e06b0e21ebea036abf506ac9f2fec4d8f99ee7
groman-gonzalez <gerald.romangonzalez@gmail.com>
2018-12-02 14:05:50 -0500

    Ttrying to fix this branch

--- MovieFanatic/src/app/app.module.ts
+++ MovieFanatic/src/app/app.module.ts
@@ -2,18 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-//import { Http } from '@angular/http';
-//import { Tabulator } from 'tabulator-tables/dist/js/tabulator.min.js';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieViewComponent } from './components/movie-view/movie-view.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
+
 import { ActorViewComponent } from 'src/app/components/actor-view/actor-view.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { Actor } from './models/actor';
 
 @NgModule({
+  
   declarations: [
     AppComponent,
     MovieViewComponent,
@@ -27,7 +28,7 @@ import { FooterComponent } from './components/footer/footer.component';
     HttpClientModule,
     AppRoutingModule,
     FormsModule
-  ],
+],
   providers: [ 
   ],
   bootstrap: [AppComponent]
