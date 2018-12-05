a0bce33fa65f65d3b5cf244a54d1b9f204a65810
Ted Airey <ted.airey4@gmail.com>
2018-12-05 13:24:21 -0500

    main-view update

--- MovieFanatic/src/app/services/movie-api.service.ts
+++ MovieFanatic/src/app/services/movie-api.service.ts
@@ -27,6 +27,14 @@ export class MovieAPIService {
     return this.http.get<MovieAPI[]>(ConfigAPI.base_url + 'discover/movie' + ConfigAPI.api_key + ConfigAPI.query_params + 'primary_release_year=2018&sort_by=vote_average.desc');
   }
 
+  getDramas() {
+    return this.http.get<MovieAPI[]>(ConfigAPI.base_url + 'discover/movie' + ConfigAPI.api_key + ConfigAPI.query_params + 'sort_by=popularity.desc&with_genres=18');
+  }
+
+  getComedies() {
+    return this.http.get<MovieAPI[]>(ConfigAPI.base_url + 'discover/movie' + ConfigAPI.api_key + ConfigAPI.query_params + 'sort_by=popularity.desc&with_genres=35');
+  }
+
   getCredits(id: string) {
     return this.http.get<CreditsAPI[]>(ConfigAPI.base_url + 'movie/' + id + '/credits' + ConfigAPI.api_key);
   }
