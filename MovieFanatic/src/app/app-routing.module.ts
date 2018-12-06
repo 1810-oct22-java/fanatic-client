6d3a79a16d31d7084ddb5ffcabd755915e19bf41
groman-gonzalez <gerald.romangonzalez@gmail.com>
2018-12-06 09:02:20 -0500

    new code for login

--- MovieFanatic/src/app/app-routing.module.ts
+++ MovieFanatic/src/app/app-routing.module.ts
@@ -8,18 +8,20 @@ import { LoginComponent } from './components/login/login.component';
 import { ActorViewComponent } from 'src/app/components/actor-view/actor-view.component';
 import { SearchViewComponent } from './components/search-view/search-view.component';
 import { MainViewComponent } from './components/main-view/main-view.component';
+import { HeaderComponent } from './components/header/header.component';
 
 const routes: Routes = [
-  { path: '', redirectTo:'main', pathMatch: 'full' },
-  { path: 'main', component: MainViewComponent},
-  { path: 'movie', component: MovieViewComponent},
-  { path: 'login', component: LoginComponent},
-  { path: 'movie_results/:query', component: SearchViewComponent},
-  { path:'movie/:id', component: MovieViewComponent },
-  { path:'movie/review/:id', component: MovieReviewComponent},
-  { path:'actor/:id', component: ActorViewComponent },
-  { path: 'profile-view', component: ProfileViewComponent},
-  { path: 'edit-profile', component: EditProfileComponent}
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: 'main', component: MainViewComponent },
+  { path: 'movie', component: MovieViewComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'movie_results/:query', component: SearchViewComponent },
+  { path: 'movie/:id', component: MovieViewComponent },
+  { path: 'movie/review/:id', component: MovieReviewComponent },
+  { path: 'actor/:id', component: ActorViewComponent },
+  { path: 'profile-view', component: ProfileViewComponent },
+  { path: 'edit-profile', component: EditProfileComponent },
+  { path: 'header', component: HeaderComponent }
 ];
 
 @NgModule({
--- MovieFanatic/src/app/components/header/header.component.ts
+++ MovieFanatic/src/app/components/header/header.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+/*login component*/
+import { LoginService } from 'src/app/services/login.service';
 
 
 @Component({
@@ -10,19 +13,38 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
   public query: string;
 
-  constructor(public router: Router) { 
+  username = '';
+  password = '';
+  output: string;
+  currentUser: User;
+
+  constructor(
+    public router: Router,
+    private loginService: LoginService) {
   }
 
   ngOnInit() {
   }
-
   /**
    * user keyed in search and we need to send it to the search results window
    */
-  enterPressed() { 
+
+  enterPressed() {
     var query: string = this.query.replace(/\s/g, '+');
-    this.query = "";
+    this.query = '';
     // change the spaces to plusses and sends it on its way
-    this.router.navigateByUrl("/movie_results/" + query);
+    this.router.navigateByUrl('/movie_results/' + query);
+  }
+
+  /*Login*/
+  login() {
+    this.currentUser = this.loginService.validateUser(this.username, this.password);
+    if (this.currentUser == null || this.currentUser === undefined) {
+      this.output = 'Invalid Credentials';
+
+    } else {
+      this.output = `Welcome ${this.username}`;
+    }
+
   }
 }
