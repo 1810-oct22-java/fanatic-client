36771d6a7e2d587702cf51d6a80be37550a9e6c3
groman-gonzalez <gerald.romangonzalez@gmail.com>
2018-12-11 13:03:35 -0500

    Working on the login service. Instead of calling the mock service, it will instead call the mvc

--- MovieFanatic/src/app/services/login.service.ts
+++ MovieFanatic/src/app/services/login.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+/*
+export class LoginService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'my-auth-token'
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  public getBook(): Observable<Book[]> {
+    return this.http.get<Book[]>('http://ec2-18-212-57-121.compute-1.amazonaws.com:8080/bookstore-mvc-0.0.1-SNAPSHOT/books');
+  }
+  public getBookById(id: string): Observable<Book> {
+    return this.http.get<Book>('http://ec2-18-212-57-121.compute-1.amazonaws.com:8080/bookstore-mvc-0.0.1-SNAPSHOT/books/' + id);
+  }
+  public updateBook(book: Book): Observable<Book> {
+    return this.http.put<Book>(Configuration.url + '/books/' + book.id, book, this.httpOptions);
+  }
+  public getAuthorById(id: string): Observable<Author> {
+    return this.http.get<Author>(Configuration.url + '/authors/' + id);
+  }
+  public updateAuthor(author: Author): Observable<Author> {
+    return this.http.put<Author>(Configuration.url + '/authors/' + author.id, author, this.httpOptions);
+  }
+
+}
+*/
 export class LoginService {
   userList: User[] = [];
 
