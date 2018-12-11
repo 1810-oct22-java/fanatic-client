febdd413c4649551fa42d96405d7ee7768de707e
pgerringer <44367095+pgerringer@users.noreply.github.com>
2018-12-10 21:42:05 -0500

    Movie view feature (#110)
    * merging...again
    
    * cleaning up dev and prod
    
    * Cleaning up again
    
    * taking out an md
    
    * attempting routing and tslint
    
    * Fixing routing
    
    * Changing to font awesom
    
    * ooops.  fixed it
    
    * Adding routerLink
    
    * routerlink changes as well as movie_view cleanup
    
    * adding code to the review view
    
    * added links to Director and Producer
    
    * Small changes.
    
    * correcting Ted's name
    
    * Changed breand behavior
    
    * adding user persistance
    
    * finalized login persistence
    
    * finalizing pre-DB run of entering a review
    
    * Implemented api call for review view
    
    * adding review functionality
    
    * Added review view processing.

--- MovieFanatic/src/app/components/movie-review/movie-review.component.ts
+++ MovieFanatic/src/app/components/movie-review/movie-review.component.ts
@@ -19,15 +19,7 @@ export class MovieReviewComponent implements OnInit {
   public ratings: OMDBAPI;
 
   // table vars
-  public dataSource = [
-    new Review(1, 'bob', '10-20-2000', 'Bobby', 2, 'or even clipped your toe nails', '10-20-2000', 2000, 50),
-    new Review(2, 'chester', '10-20-2000', 'Bobby', 3, 'or played a video game', '10-14-2018', 100, 20),
-    new Review(2, 'lester', '10-20-2000', 'Bobby', 1, 'You could have went bowling.', '10-14-2018', 100, 20),
-    new Review(2, 'carmen', '10-20-2000', 'Bobby', 4, 'I want some popcorn.', '10-14-2018', 100, 20),
-    new Review(2, 'bart', '10-20-2000', 'Bobby', 3, 'I cant believe it either.', '10-14-2018', 100, 20),
-    new Review(2, 'smoking', '10-20-2000', 'Bobby', 1, 'I cant believe I watched the whole thing.', '10-14-2018', 100, 20)
-  ];
-
+  public dataSource = [];
   public add_rating = 0;
   public add_review = '';
 
@@ -48,7 +40,21 @@ export class MovieReviewComponent implements OnInit {
                     this.movieService.getOMDB(this.movie.imdb_id).subscribe(
                       (omdb) => {
                                   this.ratings = omdb;
-                                });
+                                  this.movieService.getMovieReviews(this.movie.id, this.loginService.getUserID()).subscribe(
+                                    (reviewList) => {
+                                                  reviewList.forEach(element => {
+                                                    const review: Review = new Review(element[0], element[1], element[2], element[3],
+                                                                                      element[4], element[5], element[6],
+                                                                                      this.zero(element[7]),
+                                                                                      this.zero(element[8]));
+                                                    console.log(review);
+                                                    this.dataSource.push(review);
+                                                                                });
+                                    }
+                                  );
+                                }
+                    );
+                  console.log(this.dataSource);
                   });
   }
 
@@ -76,8 +82,8 @@ export class MovieReviewComponent implements OnInit {
       null
     );
 
-    // submit the insert
-    this.dataSource.push(new Review(100, 'schmitty', '12/8/2018', 'Venom', review.rating, review.review, '08/08/2000', 12, 24));
+    // // submit the insert
+    // this.dataSource.push(new Review(100, 'schmitty', '12/8/2018', 'Venom', review.rating, review.review, '08/08/2000', 12, 24));
 
     // clean up the local vars
     this.add_rating = 0;
@@ -86,5 +92,21 @@ export class MovieReviewComponent implements OnInit {
     // close the modal
     this.closeBtn.nativeElement.click();
     console.log(review);
+  }
+
+  zero(int: number) {
+    if (int == null) {
+      return 0;
+    }
+    return int;
+  }
+
+  postUp(review_id: number) {
+
+  }
+
+  postDown(review_id: number) {
+
+
   }
 }
--- MovieFanatic/src/app/services/movie-api.service.ts
+++ MovieFanatic/src/app/services/movie-api.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http'; 
+import { HttpModule } from '@angular/http';
 import { ConfigAPI } from 'src/app/models/configAPI';
 import { MovieAPI } from 'src/app/models/movieAPI';
 import { CreditsAPI } from 'src/app/models/creditsAPI';
 import { OMDBAPI } from 'src/app/models/OMDBAPI';
+import { Review } from '../models/review';
 import { Favorite } from 'src/app/models/favorite';
 
 @Injectable({
@@ -54,6 +57,11 @@ export class MovieAPIService {
   getOMDB(imdb_id: string) {
     return this.http.get<OMDBAPI>(ConfigAPI.omdapi_base_url + '?i=' + imdb_id +
       '&plot=full&' + ConfigAPI.omdapi_key);
+  }
+
+  getMovieReviews(movie_id: number, source_id: number) {
+    return this.http.get<Review[]>(ConfigAPI.spring_url + 'review/view/'
+      + movie_id + '/' + source_id);
   }
 
   formatPosterImage(image: string): string {
