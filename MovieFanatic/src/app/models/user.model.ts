2b0b92b554dd4a101cb88f38ec23e239c3fac56c
groman-gonzalez <gerald.romangonzalez@gmail.com>
2018-12-11 22:06:49 -0500

    Modified login in the header component ts to call the api rather than the mock service. Need to update latency.

--- MovieFanatic/src/app/models/user.model.ts
+++ MovieFanatic/src/app/models/user.model.ts
@@ -2,6 +2,7 @@ export class User {
     constructor(
         public id: number,
         public user_name: string,
+        //public username: string,
         public email: string,
         public password: string,
         public token: string
--- MovieFanatic/src/app/services/login.service.ts
+++ MovieFanatic/src/app/services/login.service.ts
@@ -1,56 +1,24 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { ConfigAPI } from '../models/configAPI';
 
 @Injectable({
   providedIn: 'root'
 })
-/*
+
 export class LoginService {
   httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': 'my-auth-token'
+      'Content-Type': 'application/json'
     })
   };
 
   constructor(private http: HttpClient) { }
 
-  public getBook(): Observable<Book[]> {
-    return this.http.get<Book[]>('http://ec2-18-212-57-121.compute-1.amazonaws.com:8080/bookstore-mvc-0.0.1-SNAPSHOT/books');
-  }
-  public getBookById(id: string): Observable<Book> {
-    return this.http.get<Book>('http://ec2-18-212-57-121.compute-1.amazonaws.com:8080/bookstore-mvc-0.0.1-SNAPSHOT/books/' + id);
-  }
-  public updateBook(book: Book): Observable<Book> {
-    return this.http.put<Book>(Configuration.url + '/books/' + book.id, book, this.httpOptions);
-  }
-  public getAuthorById(id: string): Observable<Author> {
-    return this.http.get<Author>(Configuration.url + '/authors/' + id);
-  }
-  public updateAuthor(author: Author): Observable<Author> {
-    return this.http.put<Author>(Configuration.url + '/authors/' + author.id, author, this.httpOptions);
-  }
-
-}
-*/
-export class LoginService {
-  userList: User[] = [];
-
-  constructor() {
-    this.userList.push(new User(3, 'admin', 'admin@movie.com', 'password', 'password'));
-  }
-
-  public validateUser(username: string, password: string): User {
-    for (const u of this.userList) {
-      if (u.user_name.toLowerCase() === username) {
-        if (u.password.toLowerCase() === password) {
-          this.persistLogin(u);
-          return u;
-        }
-      }
-    }
-    return null;
+  // public validateUser(username: String, password: String) {
+  public validateUser(username, password) {
+    return this.http.get<User>(ConfigAPI.spring_url + 'login/' + username + '/' + password);
   }
 
   private persistLogin(user: User) {
