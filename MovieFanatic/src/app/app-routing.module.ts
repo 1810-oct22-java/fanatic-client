93d54d91a91141b9144cca96f8aab76eecf39400
Gerald Roman <44367123+groman-gonzalez@users.noreply.github.com>
2018-12-13 18:49:30 -0500

    Login feature (#127)
    * Edit README.md
    
    * New code
    
    * new code
    
    * new fixes to the dev folder
    
    * Made an np project
    
    * This is the login page component and login service.
    
    * Ttrying to fix this branch
    
    * Login component fixes
    
    * new code for login
    
    * new code for login
    
    * Fix login component to be a modal in the header. No need to make the loginComponent in the router module
    
    * Update from master
    
    * new code from master
    
    * Working on the login service. Instead of calling the mock service, it will instead call the mvc
    
    *  Modified login in the header component ts to call the api rather than the mock service. Need to update latency.
    
    * update code from master
    
    *  updated login component
    
    * Login component now receives the correct information from the database
    
    * fix favorite movies
    
    * Deleted login component and modified movie view component
    
    * Modified movie review component
    
    * Modified the movie review component and the review model

--- MovieFanatic/src/app/app-routing.module.ts
+++ MovieFanatic/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { MovieViewComponent } from 'src/app/components/movie-view/movie-view.com
 import { ProfileViewComponent } from 'src/app/components/profile-view/profile-view.component';
 import { EditProfileComponent } from 'src/app/components/edit-profile/edit-profile.component';
 import { MovieReviewComponent } from 'src/app/components/movie-review/movie-review.component';
-import { LoginComponent } from './components/login/login.component';
 import { ActorViewComponent } from 'src/app/components/actor-view/actor-view.component';
 import { SearchViewComponent } from './components/search-view/search-view.component';
 import { MainViewComponent } from './components/main-view/main-view.component';
@@ -17,7 +16,6 @@ const routes: Routes = [
   { path: 'header', component: HeaderComponent },
   { path: 'main', component: MainViewComponent},
   { path: 'movie', component: MovieViewComponent},
-  { path: 'login', component: LoginComponent},
   { path: 'movie_results/:query', component: SearchViewComponent, runGuardsAndResolvers: 'always'},
   { path: 'movie/:id', component: MovieViewComponent},
   { path: 'movie/review/:id', component: MovieReviewComponent},
