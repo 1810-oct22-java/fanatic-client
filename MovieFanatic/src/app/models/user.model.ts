5b48a3c0cca6f02b191ae3940ea0e98f977f3511
groman-gonzalez <gerald.romangonzalez@gmail.com>
2018-12-12 20:37:40 -0500

    update code from master

--- MovieFanatic/src/app/models/user.model.ts
+++ MovieFanatic/src/app/models/user.model.ts
@@ -2,7 +2,6 @@ export class User {
     constructor(
         public id: number,
         public user_name: string,
-        //public username: string,
         public email: string,
         public password: string,
         public token: string
