1a5f30d0eeea2f2a62e03a814ad1fc70c9417153
Sean Young <seanpyoung123@gmail.com>
2018-12-14 17:42:51 -0500

    added configapi

--- MovieFanatic/src/app/models/review.ts
+++ MovieFanatic/src/app/models/review.ts
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-export class Review {
-    constructor (
-        public id: number,
-        public user_name: string,
-        public join_date: string,
-        public review_date: string,
-        public rating: number,
-        public review: string,
-        public already_reviewed: number,
-        public thumbs_up: number,
-        public thumbs_down: number
-    ) { }
-}
-=======
 export class Review {
     constructor (
         public id: number,
@@ -26,4 +11,3 @@ export class Review {
         public thumbs_down: number
     ) { }
 }
->>>>>>> 9809e184ec3770f980361ff362d145efc30afabc
