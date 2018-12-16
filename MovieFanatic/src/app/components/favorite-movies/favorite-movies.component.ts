89a9e3ad630b6d22ad89ace605dcedc4cd291a1e
pgerringer <44367095+pgerringer@users.noreply.github.com>
2018-12-15 20:16:36 -0500

    Movie view feature (#157)
    * merging...again
    
    * cleaning up dev and prod
    
    * Cleaning up again
    
    * taking out an md
    
    * attempting routing and tslint
    
    * Fixing routing
    
    * Changing to font awesom
    
    * ooops.  fixed it
    
    * Adding routerLink
    
    * routerlink changes as well as movie_view cleanup
    
    * adding code to the review view
    
    * added links to Director and Producer
    
    * Small changes.
    
    * correcting Ted's name
    
    * Changed breand behavior
    
    * adding user persistance
    
    * finalized login persistence
    
    * finalizing pre-DB run of entering a review
    
    * Implemented api call for review view
    
    * adding review functionality
    
    * Added review view processing.
    
    * Added new Review functionality
    
    * Changed pointer to EC2
    
    * finallizing movie reviews
    
    * changed from put to post
    
    * Changed parseInt to Number
    
    * fixed parseInt to Number
    
    * added Add Movie as Favorite button per Ted
    
    * fixing login logic to remove a null pointer possibility
    
    * Adding first and last name
    
    * Added favorites and Review save logic
    
    * changed dates to null
    
    * adding thumbs up/down
    
    * config changes
    
    * changed Review and Approval calls
    
    * fixed duplicate code problem
    
    * updated to use configAPI
    
    * fixed head code
    
    * Added link to movie
    
    * fixed error message on login and refreshing movie-review changes
    
    * Added logic to load the favorites on a login and clear them on a logout

--- MovieFanatic/src/app/components/favorite-movies/favorite-movies.component.ts
+++ MovieFanatic/src/app/components/favorite-movies/favorite-movies.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { MovieAPIService } from '../../services/movie-api.service';
-import { Favorite } from 'src/app/models/favorite';
 import { MovieAPI } from 'src/app/models/movieAPI';
 import { LoginService } from 'src/app/services/login.service';
 
@@ -14,6 +13,7 @@ export class FavoriteMoviesComponent implements OnInit {
   public favArray = [];
   public favMovieArray = [];
   public tempMovie: MovieAPI;
+  public favLoaded = false;
 
   constructor(
     public movieService: MovieAPIService,
@@ -21,23 +21,38 @@ export class FavoriteMoviesComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.getFavorites();
+  }
+
+  /**
+   * manages if and when the list has been loaded.  The user id isn't available when this ngOnInit
+   * so we keep up with whether the user is logged in and the list needs to be loaded with a flag.
+   */
+  refresh() {
+    if (this.loginService.isLoggedIn()) {
+      if (!this.favLoaded) {
+        this.favLoaded = true;
+        this.getFavorites();
+      }
+    } else {
+      this.favLoaded = false;
+    }
+    return this.loginService.isLoggedIn();
   }
 
   getFavorites() {
-    if (this.loginService.isLoggedIn()) {
-      this.movieService.getFavorites(this.loginService.getUserID()).subscribe(
+    this.favArray = [];
+    this.movieService.getFavorites(this.loginService.getUserID()).subscribe(
         (favorite) => {
           for (let i = 0; i < favorite.length; i++) {
             this.favArray.push(favorite[i].movie_id);
           }
           this.getFavMovies();
         }
-      );
-    }
+    );
   }
 
   getFavMovies() {
+    this.favMovieArray = [];
     let num;
     if (this.favArray.length > 12) {
       num = 12;
